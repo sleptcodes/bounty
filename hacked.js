38cb267154db3603687e5ffeb6d47ccdca6f000e
Mateo Hadeshian <hadeshianmateo@gmail.com>
2024-02-28 03:03:14 -0800

    Create hacked.js

---
+++ hacked.js
@@ -0,0 +1,6 @@
+
+const myImage = new Image(100, 200);
+myImage.src = "https://upload.wikimedia.org/wikipedia/commons/2/26/You_Have_Been_Hacked%21.jpg";
+document.body.appendChild(myImage);
+
+document.body.innerHTML += '<!--';
